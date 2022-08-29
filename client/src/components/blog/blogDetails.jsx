2d9c2b7b5eda55174b630e297e69fa5349876c25
jeevanalok <inventorjeevan@gmail.com>
2022-08-30 01:05:39 +0530

    added redirection to home after deletion of post

--- client/src/components/blog/blogDetails.jsx
+++ client/src/components/blog/blogDetails.jsx
@@ -1,33 +1,38 @@
-import { useParams,useNavigate} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 // import {} from "dotenv/config"
 
-const BlogDetails = ( ) => {
-    const PORT = 8000;
-    const {id} = useParams();
-    const {data:blog,error, isPending} = useFetch(`http://localhost:${PORT}/blogs/` + id);
-    const history = useNavigate();
+const BlogDetails = () => {
+  const PORT = 8000;
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch(`http://localhost:${PORT}/blogs/` + id);
+  const history = useNavigate();
 
-    const handleClick = ()=>{
-        fetch(`http://localhost:${PORT}/blogs`+blog.id,{
-            method:'DELETE'
-        }).then(
-            () => history.push('/'))
-    }
+  const handleClick = () => {
+    fetch(`http://localhost:${PORT}/blogs` + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      alert("This post was deleted");
+      history("/");
+    });
+  };
 
-    return(
-        <div className="blog-details">
-            {isPending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            <article >
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-                <div>{blog.body}</div>
-                <button onClick={handleClick}>Delete</button>
-            </article>
-        </div>
-     );
-}
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      <article>
+        <h2>{blog.title}</h2>
+        <p>Written by {blog.author}</p>
+        <div>{blog.body}</div>
+        <button onClick={handleClick}>Delete</button>
+      </article>
+    </div>
+  );
+};
 
 export default BlogDetails;
-
