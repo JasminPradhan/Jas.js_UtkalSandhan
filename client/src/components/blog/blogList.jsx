8b7a4fd0ef0605f23cb57d62ec9acd50f664c946
jeevanalok <inventorjeevan@gmail.com>
2022-08-29 19:44:01 +0530

    made blog fetch data from backend and integrated

--- client/src/components/blog/blogList.jsx
+++ client/src/components/blog/blogList.jsx
@@ -1,24 +1,9 @@
 import {Link} from 'react-router-dom';
-// import {} from "dotenv/config"
 
-const BlogList =  async ({title}) =>{
+
+const BlogList =  ({title}) =>{
     const PORT = 3000;
 
-    // const response = await fetch(`http://localhost:${PORT}`)
-    const blogs = [
-        {
-            "title": "new title ",
-            "body": "hackathon code4odisha",
-            "author": "jeevan",
-            "id": 4
-          },
-          {
-            "title": "lol",
-            "body": "lol",
-            "author": "lol",
-            "id": 5
-          }
-    ]
     return(
         <div className="blog-list">
             <h2>{title}</h2>
--- client/src/components/blog/home.jsx
+++ client/src/components/blog/home.jsx
@@ -1,12 +1,18 @@
 // import useFetch from "./useFetch";
+import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 function Home() {
+  const [blogData, setBlogData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/blogs")
+      .then((response) => response.json())
+      .then((data) => setBlogData(data));
+  }, []);
 
   return (
     <div>
-
-      <BlogList title="All Blogs" />
-
+      <BlogList blogs={blogData} title="All Blogs" />
     </div>
   );
 }
