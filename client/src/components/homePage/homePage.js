7c8a12fa932a897d9219ffd3e24532caad745521
jasmin <jasminpradhan949@gmail.com>
2022-11-08 11:45:45 +0530

    minor changes

--- client/src/components/homePage/homePage.js
+++ client/src/components/homePage/homePage.js
@@ -20,13 +20,36 @@ const Landing=()=>{
         <div>
             <div>
                 <img src={sec1} alt="" className="relative " />
-                <img src={logo} alt="logo" className="absolute mt-10 ml-10 top-[9px] left-[0px] object-cover object-right  h-[100px] w-[200px] inline" />
-            <div className="flex flex-row space-x-6 absolute top-[9px] left-[900px] text-yellow-700">
-                <div className="">
-
-
-                    <div className="flex flex-row space-x-6 mt-[60px]">
-                        <div className="cursor-pointer" onClick={userProfile}>Profile</div>
+                <img 
+                src={logo} 
+                alt="logo" 
+                className="
+                absolute 
+                mt-10 
+                ml-10 
+                top-[1rem] 
+                lg:top-[-.5rem] 
+                left-[0rem] 
+                lg:left-[-1rem] 
+                object-cover 
+                object-right  
+                h-[7rem] 
+                w-[14rem] 
+                inline" />
+            <div 
+            className="
+            flex 
+            flex-row 
+            space-x-6 
+            absolute 
+            top-[1rem] 
+            lg:top-[-.5rem] 
+            left-[40rem] 
+            xl:left-[65rem]
+            text-yellow-700">
+                <div>
+                    <div className="flex flex-row space-x-6 mt-[5rem]">
+                        <div className="cursor-pointer" onClick={userProfile}>Login</div>
                         <div className="cursor-pointer" onClick={userProfile}>Wishlist</div>
                         <div className="cursor-pointer" onClick={userProfile}>Notification</div>
                         <div className="cursor-pointer" onClick={createBlog}>Publish</div>
@@ -38,25 +61,89 @@ const Landing=()=>{
             </div>
             <div>
                 <img src={sec2} alt="" className="relative"/>
-                <button className="bg-white absolute h-[40px] text-lg w-[220px] top-[1210px] left-[135px] rounded-full">Read More</button>
+                <button 
+                className="
+                bg-white 
+                absolute
+                h-[2.1rem]
+                xl:h-[2.3rem]
+                text-lg 
+                w-[9rem] 
+                top-[56.8rem] 
+                lg:top-[59.5rem] 
+                xl:top-[84rem]
+                left-[5.45rem] 
+                xl:left-[8rem]
+                rounded-full">
+                    Read More
+                </button>
             </div>
             <div>
                 <img src={sec3} alt="" className="relative" />
-                <button className="bg-white absolute h-[40px] text-lg w-[200px] top-[1976px] right-[306px] rounded-full">Read More</button>
+                <button 
+                className="
+                bg-white 
+                absolute 
+                h-[2.1rem] 
+                xl:h-[2.3rem]
+                text-lg 
+                w-[9rem] 
+                top-[91.5rem] 
+                lg:top-[95.5rem]
+                xl:top-[134.81rem]
+                right-[14.85rem]
+                lg:right-[15.8rem]
+                xl:right-[25.6rem]
+                rounded-full">
+                    Read More
+                </button>
 
             </div>
             <div>
                 <img src={sec4} alt="" className="relative" />
                 <div className="">
                     <form>
-                        <input type="text" placeholder="Search for more places.. " className=" bg-white absolute h-[40px] text-lg w-[380px] text-center top-[2880px] left-[470px] rounded-full" />
+                        <input 
+                        type="text" 
+                        placeholder="Search for more places.. " 
+                        className=" 
+                        bg-white 
+                        absolute 
+                        h-[2.5rem] 
+                        text-lg 
+                        w-[25rem] 
+                        lg:w-[35rem] 
+                        lg:w-[40rem]
+                        text-center 
+                        top-[134rem] 
+                        lg:top-[139.5rem] 
+                        xl:top-[197rem]
+                        left-[18.5rem] 
+                        lg:left-[15rem] 
+                        xl:left-[25rem]
+                        rounded-full" />
                     </form>
                 </div>
                 
             </div>
             <div>
                 <img src={sec5} alt="" className="relative" />
-                <button className="bg-white absolute h-[40px] text-lg w-[220px] top-[3590px] left-[130px] rounded-full">Share your trip</button>
+                <button 
+                className="
+                bg-white 
+                absolute 
+                h-[2.1rem]
+                xl:h-[2.3rem] 
+                text-lg 
+                w-[11rem] 
+                top-[155rem] 
+                lg:top-[162rem] 
+                xl:top-[228rem]
+                left-[6rem] 
+                xl:left-[8.7rem]
+                rounded-full">
+                    Share your trip
+                </button>
             </div>
         </div>
     );
